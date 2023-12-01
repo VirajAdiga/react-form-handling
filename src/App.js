550e3dc1bd2a49760d147c44b49ac1317fdff11c
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-01 11:08:14 +0530

    Add book edit functionality

--- src/App.js
+++ src/App.js
@@ -9,9 +9,29 @@ function App() {
     setBooks([...books, {id: Math.round(Math.random() * 9999), title: title}]);
   }
 
+  const handleDeleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    })
+    setBooks(updatedBooks);
+  }
+
+  const handleEditBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id){
+        return {
+          ...book,
+          title: newTitle
+        };
+      }
+      return book;
+    })
+    setBooks(updatedBooks);
+  }
+
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={handleDeleteBookById} onEdit={handleEditBookById} />
       <BookCreate onCreate={handleCreate} />
     </div>
   );
