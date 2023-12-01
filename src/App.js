fa451a4c592920c7bb80345de099ce04d1477a40
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-01 09:51:52 +0530

    Add and update App component to have BokCreate and BookList components, handle book updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList'
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const handleCreate = (title) => {
+    setBooks([...books, {id: Math.round(Math.random() * 9999), title: title}]);
+  }
+
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <BookList books={books} />
+      <BookCreate onCreate={handleCreate} />
     </div>
   );
 }
